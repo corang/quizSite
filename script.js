84a848b6f89709d50d629c6fece2edb1233a816d
Jordan McClintock <jordanmc12@verizon.net>
2017-04-29 21:38:59 -0500

    Working on questions

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ myApp.controller('Question', ['$scope', function ($scope) {
     var question2 = ['2answer 1', '2answer 2', '2answer 3', '2answer 4'];
 
     $scope.questions = [];
-    $scope.questions[0] = ['test question', question1];
-    $scope.questions[1] = ['test question2', question2];
+    $scope.questions[0] = ['test question', question1, 1];
+    $scope.questions[1] = ['test question2', question2, 3];
     $scope.currentQuestion = 0;
+
+    $scope.checkAnswer = function () {
+
+    }
 }]);
