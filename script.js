2fbffcd598a1414ce00bd6a39aaf0135ef3199fe
Jordan McClintock <jordanmc12@verizon.net>
2017-04-29 20:43:26 -0500

    Formatting and Adding question ng stuff

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-﻿var myApp = angular.module('myApp', []);
+﻿var myApp = angular.module('Quiz', []);
 
-myApp.controller('GreetingController', ['$scope', function ($scope) {
-    
+myApp.controller('Question', ['$scope', function ($scope) {
+    var question1 = ['answer 1', 'answer 2', 'answer 3', 'answer 4'];
+    var question2 = ['2answer 1', '2answer 2', '2answer 3', '2answer 4'];
+
+    $scope.questions = [];
+    $scope.questions[0] = ['test question', question1];
+    $scope.questions[1] = ['test question2', question2];
+    $scope.currentQuestion = 0;
 }]);
