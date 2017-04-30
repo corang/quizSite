ae619379122c0002a2651783a14bd366002c031f
Jordan McClintock <jordanmc12@verizon.net>
2017-04-29 22:53:17 -0500

    Added CS program for creating quizzes

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ myApp.controller('Question', ['$scope', function ($scope) {
     $scope.questions[0] = ['test question', question1, 1];
     $scope.questions[1] = ['test question2', question2, 3];
     $scope.currentQuestion = 0;
+    $scope.qCount = $scope.questions.length;
+    
 
-    $scope.checkAnswer = function () {
-
+    $scope.checkAnswer = function (selectedAnswer) {
+        if (parseInt(selectedAnswer) === parseInt($scope.questions[$scope.currentQuestion][2])) {
+            console.log('correct');
+            if ((parseInt($scope.currentQuestion) + 1) === parseInt($scope.qCount)) {
+                document.getElementById('question').style.display = 'none';
+                document.getElementById('done').style.removeProperty('display');
+            } else {
+                $scope.currentQuestion++;
+            }
+        } else {
+            console.log('incorrect')
+        }
+        console.log($scope.currentQuestion);
     }
 }]);
